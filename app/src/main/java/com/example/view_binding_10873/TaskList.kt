080bfa619f1eb0c8b3e353c9cec6ccde1be97e71
EnriqueTrisfan <enriquetrisfan01@gmail.com>
2022-09-15 08:32:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10873/TaskList.kt
@@ -0,0 +1,17 @@
+package com.example.view_binding_10873
+
+object TaskList {
+    val tasklist = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan  sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Bingung",
+            "Bingung dengan apa yang diketik asdos"),
+        Task("Chapter 3", "Ngopi",
+            "Habis capek bingung, kan enak ngopi ya kan"),
+        Task("Chapter 4", "Heran",
+            "Aduk Mekanik Bom Bakudan klee sama Bom anu"),
+        Task("Chapter 5", "Yauda",
+            "Yauda pokonya gt dah"),
+
+        )
+}
